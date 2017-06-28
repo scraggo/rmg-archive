143fd3543b2c7bb0f2a2d9864ffa1c5cbe905de1
Steven Bigham <Steven@Bigham.com>
2017-06-29 00:52:03 +0100

    Removed globals

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,6 @@
 var MidiWriter = require('midi-writer-js');
 
-window.hotCrossBun = function() {
+function hotCrossBun() {
   var track = new MidiWriter.Track();
 
   track.addEvent([
@@ -27,7 +27,7 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-window.trulyRandom = function() {
+function trulyRandom() {
   var track = new MidiWriter.Track();
 
   let noteArray = [];
@@ -59,21 +59,15 @@ window.trulyRandom = function() {
 
 
 //melody test
-window.melodyTest = function() {
+function melodyTest() {
   var track = new MidiWriter.Track();
   let noteArray = [];
   let melody = ['↑5', '↑6', '↓5', '↑2', '↑1', '↓5', '↓5', '↓2'];
   let pitchMap = { '1': 'C', '2': 'D', '3': 'E', '4': 'F', '5': 'G', '6': 'A', '7': 'B'};
   let duration = 2;
   
-  console.log(melody[0]);
-  console.log(melody[0][1]);
-  console.log(melody[0][3]);
-  console.log(melody[0].split(""));
-
-
   for (let i=0; i<melody.length; i++) {
-    let pitchNum = melody[i][1];//the arrow is some weird $#% thing
+    let pitchNum = melody[i][1]; 
     let pitchLetter = pitchMap[pitchNum];
     let pitch = [pitchLetter + '4']
     console.log(pitch, duration);
@@ -89,5 +83,10 @@ window.melodyTest = function() {
 
   return write.dataUri();
 }
+
+//let notes =  C,C#/Db,D,D#/Eb,E,F,F#/Gb,G,G#/Ab,A,A#/Bb,B,
+document.getElementById("app").innerHTML = `<a href="${hotCrossBun()}" download="Hot Cross Bun.midi">Hot Cross Bun</a>`;
+document.getElementById("app").innerHTML += `<br><a href="${trulyRandom()}" download="Truly Random.midi">Truly Random</a>`;
+document.getElementById("app").innerHTML += `<br><a href="${melodyTest()}" download="melodyTest.midi">Melody Test</a>`;    
 
 /**/
