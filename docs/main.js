a688e59599a0c7b523896242caaf63dcac65a81e
Steven Bigham <Steven@Bigham.com>
2017-06-29 01:18:00 +0100

    Auto stash before merge of "master" and "scraggo/master"

--- docs/main.js
+++ docs/main.js
@@ -59,18 +59,22 @@ function trulyRandom() {
 
 
 //melody test
-function melodyTest() {
+function melodyTest(randomMelody=undefined) {
   var track = new MidiWriter.Track();
   let noteArray = [];
   let melody = ['↑5', '↑6', '↓5', '↑2', '↑1', '↓5', '↓5', '↓2'];
   let pitchMap = { '1': 'C', '2': 'D', '3': 'E', '4': 'F', '5': 'G', '6': 'A', '7': 'B'};
   let duration = 2;
+
+  if (randomMelody) {
+    melody = randomMelody.split(" ");
+  }
   
   for (let i=0; i<melody.length; i++) {
     let pitchNum = melody[i][1]; 
     let pitchLetter = pitchMap[pitchNum];
     let pitch = [pitchLetter + '4']
-    console.log(pitch, duration);
+    //console.log(pitch, duration);
     let note = new MidiWriter.NoteEvent({pitch, duration});
     noteArray.push(note);
   }
@@ -85,8 +89,31 @@ function melodyTest() {
 }
 
 //let notes =  C,C#/Db,D,D#/Eb,E,F,F#/Gb,G,G#/Ab,A,A#/Bb,B,
+
 document.getElementById("app").innerHTML = `<a href="${hotCrossBun()}" download="Hot Cross Bun.midi">Hot Cross Bun</a>`;
 document.getElementById("app").innerHTML += `<br><a href="${trulyRandom()}" download="Truly Random.midi">Truly Random</a>`;
 document.getElementById("app").innerHTML += `<br><a href="${melodyTest()}" download="melodyTest.midi">Melody Test</a>`;    
 
-/**/
+
+let direction = ["↑", "↓"];
+
+function melody_d(notes) {
+  let melody = "";
+  for (let x = 0; x < notes; x++) {
+    let randDirection = direction[Math.floor(Math.random()*direction.length)];
+    let diatonic7 = getRandomInt(1, 7);
+    melody += `${randDirection}${diatonic7} `;
+  }
+  return melody.slice(0, melody.length-1);
+} // Direction + Number
+
+let button = document.createElement("button");
+button.innerHTML = "New Random Melody";
+button.addEventListener("click", newRandom);
+document.body.append(button);
+
+function newRandom() {
+  let newRandomMelody = melody_d(10);
+  console.log(newRandomMelody);
+  document.getElementById("app").innerHTML += `<br>${newRandomMelody} - <a href="${melodyTest(melody_d(10))}" download="melodyTest.midi">Random Melody Test</a>`;
+}
